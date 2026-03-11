6e85f1583e866a0dc1de1e3a2d9adf57f8dd3299
infamousflame <67448147+infamousflame@users.noreply.github.com>
2026-03-11 14:11:36 +1300

    Update model.rs

--- src/model.rs
+++ src/model.rs
@@ -3,7 +3,7 @@ use crate::datatable::{Column, DataTable, Value};
 
 pub trait SupervisedModel {
     fn new() -> Self;
-    fn train(&mut self, X: &DataTable, y: &Column);
+    fn fit(&mut self, X: &DataTable, y: &Column);
     fn predict(&self, row: &Vec<Value>) -> Value;
     fn accuracy(&self, X: &DataTable, y: &Column) -> f64;
 }
