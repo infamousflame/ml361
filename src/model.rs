9b39e46bc47cd89a15e46cd693b8f666f481484a
infamousflame <67448147+infamousflame@users.noreply.github.com>
2026-03-13 13:19:58 +1300

    Remove most metrics from SupervisedModel.

--- src/model.rs
+++ src/model.rs
@@ -5,8 +5,4 @@ pub trait SupervisedModel {
     fn fit(&mut self, X: &DataTable, y: &Column) -> Result<(), &'static str>;
     fn predict(&self, row: &Vec<Value>) -> Result<Value, &'static str>;
     fn accuracy(&self, X: &DataTable, y: &Column) -> Result<f64, &'static str>;
-    fn sensitivity(&self, X: &DataTable, y: &Column) -> Result<f64, &'static str>;
-    fn specificity(&self, X: &DataTable, y: &Column) -> Result<f64, &'static str>;
-    fn precision(&self, X: &DataTable, y: &Column) -> Result<f64, &'static str>;
-    fn recall(&self, X: &DataTable, y: &Column) -> Result<f64, &'static str>;
 }
